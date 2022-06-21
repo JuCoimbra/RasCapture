ac2a17efa80cb02be31d62d34511a5481a6164a0
Juliane Coimbra <julianearc@gmail.com>
2022-06-21 16:52:07 -0300

    Códigop atualizado, agora é possível vizualizar a imagem do link. Para sair da imagem, clique em qualquer tecla.

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-from urllib import request
+import  urllib.request
 import cv2
 import numpy as np
 import urllib3
-import traceback
+
 
 #Lê a URL da imagem e carrega com urllib3
 while True:
     #Iniciando uma conexão WEB
-    http = urllib3.PoolManager()
+    #http = urllib3.PoolManager()
     
     #Pedindo a URL ao usuário
     imageUrl = input('Insira a url da imagem a ser examinada: ')
 
     #Enquanto uma URL valida não é colocada, o loop segue
     try:
-        loadURLImage = http.urlopen('GET', imageUrl)
+        loadURLImage = urllib.request.urlopen(imageUrl)
 
     #Erro no requerimento web
     except Exception as e:
@@ -27,4 +27,7 @@ while True:
 #Para que a OpenCV2 leia a imagem, é preciso converte-la em uma array uint8
 #Mais informações 
 #https://professor.luzerna.ifc.edu.br/ricardo-antonello/wp-content/uploads/sites/8/2017/02/Livro-Introdu%C3%A7%C3%A3o-a-Vis%C3%A3o-Computacional-com-Python-e-OpenCV-1.pdf
-imageArray = np.asarray(bytearray(loadURLImage.read()), dtype=np.uint8)
+imageArray = np.array(bytearray(loadURLImage.read()), dtype=np.uint8)
+image = cv2.imdecode(imageArray, -1)
+cv2.imshow('image', image)
+cv2.waitKey()
