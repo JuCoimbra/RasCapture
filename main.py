650210162bed2e6e8db1b17e728bc6140cacf391
ana-2004 <107512257+ana-2004@users.noreply.github.com>
2022-06-25 18:40:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import  urllib.request #Para requerimentos WEB na busca da imagem
 import cv2  #faz o tratamento da imagem
 import numpy as np
 import pytesseract #consegue ler os caracteres da imagem
@@ -9,43 +8,10 @@ import sys #Para forçar a saida do programa
 print('''Seja bem-vindo(a). Esse programa lê imagens com placas antigas e armazena seus dados em um banco de dados.\n
     Ao longo do programa a imagem selecionada ira ser mostrada em tela, de forma que, para sair da imagem, você deve clicar
     em qualquer tecla. Vamos começar.''')
-
-#Lê a URL da imagem e carrega com urllib3
-while True:
-    #Iniciando uma conexão WEB
-    #http = urllib3.PoolManager()
-    
-    #Pedindo a URL ao usuário
-    imageUrl = input('Insira a url da imagem a ser examinada ou X para encerrar o programa: ')
-
-    #Enquanto uma URL valida não é colocada, o loop segue
-    try:
-        loadURLImage = urllib.request.urlopen(imageUrl)
-
-    #Erro no requerimento web
-    except Exception as e:
-        if imageUrl.upper() == 'X':
-            sys.exit('Encerrando programa...\nAté a próxima!')
-        else:
-            print("Ocorreu um erro. Por favor, tente novamente.")
-    else:
-        #Imagens são matrizes cujo elemneto de preenchimento é a variavel do tipo uint8
-        #uint8 são variaveis com valores entre 0 e 255 que definem a cor do pixel
-        #Para que a OpenCV2 leia a imagem, é preciso converte-la em uma array uint8
-        #Mais informações 
-        #https://professor.luzerna.ifc.edu.br/ricardo-antonello/wp-content/uploads/sites/8/2017/02/Livro-Introdu%C3%A7%C3%A3o-a-Vis%C3%A3o-Computacional-com-Python-e-OpenCV-1.pdf
-        imageArray = np.array(bytearray(loadURLImage.read()), dtype=np.uint8)
-        image = cv2.imdecode(imageArray, -1)
-        cv2.imshow('Image', image)
-        cv2.waitKey()
-        cv2.destroyAllWindows()
-        resp = input('Essa é a imagem escolhida? Aperte:\nY - Sim\nN- Não')
-        if resp.upper() == 'Y' or '':
-            break
-        elif resp.upper() == 'N' or 'NÃO' or 'NAO':
-            continue
-        else: 
-            print('Não entendemos sua resposta, vamos tentar movamente.')
+image= cv2.imread('placa1.jpeg') 
+cv2.imshow('Image', image)
+cv2.waitKey()
+cv2.destroyAllWindows()
 
 #Tratamento da imagem
 #Transforma a imagem em tons de cinza
