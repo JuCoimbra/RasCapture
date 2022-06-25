835bb2de9830b7ac552426231aa61ba8c672b594
ana-2004 <107512257+ana-2004@users.noreply.github.com>
2022-06-25 19:30:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ from PIL import Image  #voltada para manipulação de imagens em outros formatos
 import databaselib #Biblioteca criada para o banco de dados
 
 print('''Seja bem-vindo(a). Esse programa lê imagens com placas antigas e armazena seus dados em um banco de dados.\n
-    A imagem da placa ira ser mostrada em tela, de forma que, para sair da imagem, você deve clicar
-    em qualquer tecla. Vamos começar.''')
+    Ao longo do programa a imagem selecionada irá ser mostrada em tela, de forma que, para sair da imagem, você deve 
+    clicar em qualquer tecla.
+    Se quiser ler outras placas, altere o nome do arquivo para um dos outros nomes contidos na pasta. Vamos começar!''')
+
 image= cv2.imread('placa1.jpeg') 
 cv2.imshow('Image', image)
 cv2.waitKey()
@@ -20,7 +22,14 @@ cinza = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 #em preto e branco e a otsu elima as interferências/ruídos na imagem
 blur=cv2.GaussianBlur(cinza,(3,3),0)
 final = cv2.threshold(blur, 0, 255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-cv2.imshow("final", final)                                                       
+tratando= cv2.imread('IMG1.png')
+tamanho= cv2.resize(tratando, (400,200))
+cv2.imshow('tratando', tamanho)
+cv2.waitKey()
+cv2.destroyAllWindows()
+cv2.imshow('Imagem tratada', final) 
+cv2.waitKey()
+cv2.destroyAllWindows()                                                        
 
 #Configuração da forma de leitura dos caracteres, de forma que leia números e letras, a partir da função custom_config
 
